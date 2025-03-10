3a5083eff136cd33e5a91dfccf4a78794cd80a08
salman-ctrl <zirmanvictory@gmail.com>
2025-03-10 22:12:14 +0700

    blockchain.js

--- src/Component/RegisterForm.jsx
+++ src/Component/RegisterForm.jsx
@@ -15,18 +15,11 @@ function RegisterForm() {
     };
 
     return (
-        <div className="flex flex-col items-center gap-4 p-6 bg-gray-100 rounded-lg">
-            <input
-                type="text"
-                className="border p-2 rounded w-80"
-                placeholder="Masukkan nama Anda"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-            />
-            <button className="bg-blue-500 text-white px-4 py-2 rounded" onClick={handleRegister}>
-                Daftar
-            </button>
-        </div>
+
+        <button className="bg-blue-500 text-white px-4 py-2 rounded" onClick={handleRegister}>
+            Daftar
+        </button>
+        </div >
     );
 }
 
