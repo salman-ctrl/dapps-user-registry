dbac3d1da31134c480c9aa1d1617cdc507f692ce
salman-ctrl <zirmanvictory@gmail.com>
2025-03-17 23:59:15 +0700

    edit

--- src/Component/RegisterForm.jsx
+++ src/Component/RegisterForm.jsx
@@ -14,15 +14,19 @@ function RegisterForm() {
         }
     };
 
-    return (Masukkan nama Anda"
-    value = { name }
-    onChange = {(e) => setName(e.target.value)
-}
+    return (
+        <div className="flex flex-col items-center gap-4 p-6 bg-gray-100 rounded-lg">
+            <input
+                type="text"
+                className="border p-2 rounded w-80"
+                placeholder="Masukkan nama Anda"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
             />
-    < button className = "bg-blue-500 text-white px-4 py-2 rounded" onClick = { handleRegister } >
-        Daftar
-            </button >
-        </div >
+            <button className="bg-blue-500 text-white px-4 py-2 rounded" onClick={handleRegister}>
+                Daftar
+            </button>
+        </div>
     );
 }
 
