af83cec66fde14b9bfcb627125850fbf6d58c28f
salman-ctrl <zirmanvictory@gmail.com>
2025-03-17 23:58:25 +0700

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,10 @@ import UserInfo from "./Component/UserInfo";
 
 function App() {
   return (
-    <div className="flex flex-col items-center gap-8 min-h-screen justify-center">
       <h1 className="text-2xl font-bold">User Registry DApp</h1>
       <RegisterForm />
       <UserInfo />
-    </div>
+    </div >
   );
 }
 
